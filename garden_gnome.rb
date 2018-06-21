98e1470e356b12d4a34f441d154b11a236d17d7c
coyalo03 <coyalo86@gmail.com>
2018-06-21 21:22:27 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,36 @@
-# Code your instances here
+class GardenGnome 
+    
+    attr_accessor :gluten_allergy, :hat_color, :age
+    
+    attr_reader :gnaw, :shout
+    
+    def initialize(gluten_allergy,hat_color, gnaw = "Gnawing on a Tree!", shout = "Gnarly!")
+        @gluten_allergy = gluten_allergy
+        @hat_color = hat_color 
+        @age = age
+        @gnaw = gnaw 
+        @shout = shout
+    end 
+    
+end 
+
+gnome1 = GardenGnome.new("")
+gnome2 = GardenGnome.new("")
+
+walter_the_worst = GardenGnome.new("")
+# james_the_jerk = GardenGnome.new("")
+# alfred the abhorrent = GardenGnome.new("")
+
+puts 
+puts 
+
+
+def shout
+  puts "shout"
+end
+
+def introduce_self
+  puts introduce_self
+end
+
+
